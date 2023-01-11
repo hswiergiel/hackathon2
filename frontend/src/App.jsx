acfe34e90f68d3a7cdd28e8798217324129393ac
hswiergiel <h.swiergiel@hotmail.fr>
2023-01-11 09:44:25 +0100

    reset css and sass installed"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
+import "reset-css";
+import "./style.scss";
 import Home from "./pages/Home";
-
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
     </div>
   );
 }
