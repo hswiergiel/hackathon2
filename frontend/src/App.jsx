8c98b478f1e45635f650c7337e6a380c39f2550b
Fostrankill <romain.pourchaux@gmail.com>
2023-01-11 19:09:29 +0100

    AmazonLockerArticle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import AmazonLocker from "@components/amazonLocker/AmazonLocker";
 import Home from "./pages/Home";
 
 import "./App.css";
@@ -5,8 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <AmazonLocker />
     </div>
   );
 }
