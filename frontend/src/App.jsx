2b6961e042fb50f7da9f305a96db672ebefcb7b1
cartige <cartiaux.gerald@gmail.com>
2023-01-12 15:27:05 +0100

    context log

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "@components/Navbar/Navbar";
+import { LogContextProvider } from "./contexts/LogContext";
 import Home from "./pages/Home/Home";
 import UserPage from "./pages/UserPage/UserPage";
 import RenterPage from "./pages/RenterPage/RenterPage";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <LogContextProvider>
+      <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user-page" element={<UserPage />} />
-          <Route path="/renter-page" element={<RenterPage />} />
-        </Routes>
-      </div>
-    </Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user-page" element={<UserPage />} />
+            <Route path="/renter-page" element={<RenterPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </LogContextProvider>
   );
 }
 
